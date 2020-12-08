5798ece3bb6af4725d07beb10daf8a5569e42ea9
Flavio Henrique <f.henrique.n.l@gmail.com>
2020-12-08 11:34:12 -0300

    Ajuste

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios'; 
 require('dotenv').config();
 const api = axios.create({
-  baseURL: "http://localhost:3333/"
+  // baseURL: "http://localhost:3333/"
+  baseURL: "https://controlepatrimoniosms-backend.herokuapp.com/"
 });
 
 export default api;
