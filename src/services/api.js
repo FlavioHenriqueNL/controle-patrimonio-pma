92406b3ad2b7c94dd25f843e2b2bab3c109ac3cd
Flavio Henrique <f.henrique.n.l@gmail.com>
2020-12-11 09:58:56 -0300

    Ajuste api

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'; 
-require('dotenv').config();
 const api = axios.create({
-  baseURL: "http://localhost:3333/"
+  // baseURL: process.env.REACT_APP_BASE_URL
+  baseURL: "http://localhost:3333"
 });
 
 export default api;
