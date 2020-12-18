7ad32ef051e0c869f473f3ff5d7069f3763b23ea
Flavio Henrique <f.henrique.n.l@gmail.com>
2020-12-18 12:17:41 -0300

    Finalização da primeira versão do Menu Lateral, restando apenas a adição de funcionalidades.

--- src/Components/Navbar/MenuLateral.js
+++ src/Components/Navbar/MenuLateral.js
@@ -1,11 +1,14 @@
-import {Drawer, List, Divider, ListItem, ListItemText, ListItemIcon, Collapse} from '@material-ui/core';
-import {Link, useHistory} from 'react-router-dom';
-import {FiMenu, FiPlus} from 'react-icons/fi';
+import {FiMenu, FiPlus, FiLogOut} from 'react-icons/fi';
 import {FaExchangeAlt, FaListAlt} from 'react-icons/fa';
 import {HiOutlineDesktopComputer} from 'react-icons/hi';
 import {MdExpandLess, MdExpandMore} from 'react-icons/md';
 import {GrClose} from 'react-icons/gr';
+import {AiFillHome} from 'react-icons/ai';
+import {FcStatistics} from 'react-icons/fc';
+
 import React, {useState} from 'react';
+import {Drawer, List, Divider, ListItem, ListItemText, ListItemIcon, Collapse} from '@material-ui/core';
+import {Link, useHistory} from 'react-router-dom';
 import './style.css';
 
 const MenuLateral = () => {
@@ -27,12 +30,18 @@ const MenuLateral = () => {
       <FiMenu onClick={() => setIsOpen(true)} className="menu-icon"/>
 
       <Drawer className="drawer" open={isOpen} onClose={() => setIsOpen(false)}>
-        <List>
+        <List className="list">
           <ListItem>
             <GrClose className="closeDrawer" onClick={() => setIsOpen(false)}/>
             <Divider/>
           </ListItem>
 
+          <ListItem className="listItem" button onClick={() => history.push('/')}>
+            <ListItemIcon className="listIcon"><AiFillHome/></ListItemIcon>
+            <ListItemText primary="Dashboard"/>
+          </ListItem>
+          
+          {/* PATRIMONIOS */}
           <ListItem className="listItem" button onClick={() => handleCollapse('patrimonio')}>
             <ListItemIcon className="listIcon"><HiOutlineDesktopComputer/></ListItemIcon>
             <ListItemText primary="Patrimônios" />
@@ -52,11 +61,39 @@ const MenuLateral = () => {
           </Collapse>
           <Divider/>
 
-          <ListItem>
-            <Link to="/movimentacoes">
-              <FaExchangeAlt/> Movimentações
-            </Link>
+          {/* MOVIMENTAÇÕES */}
+          <ListItem className="listItem" button onClick={() => handleCollapse('movimentacao')}>
+            <ListItemIcon className="listIcon"><FaExchangeAlt/></ListItemIcon>
+            <ListItemText primary="Movimentações" />
+            {collapsed.movimentacao ? <MdExpandLess/> : <MdExpandMore/>}
           </ListItem>
+          <Collapse className="listCollapse" in={collapsed.movimentacao} timeout="auto" unmountOnExit>
+            <List component="div" disablePadding>
+              <ListItem className="listItem" button onClick={() => history.push('movimentacao/novo')}>
+                <ListItemIcon className="listIcon"><FiPlus/></ListItemIcon>
+                <ListItemText primary="Adicionar Movimentação" />
+              </ListItem>
+              <ListItem className="listItem" button onClick={() => history.push('movimentacoes')}>
+                <ListItemIcon className="listIcon"><FaListAlt/></ListItemIcon>
+                <ListItemText primary="Lista de Movimentações" />
+              </ListItem>
+            </List>
+          </Collapse>
+          <Divider/>
+
+          <ListItem className="listItem" button onClick={() => alert('Funcionalidade em desenvolvimento')}>
+            <ListItemIcon className="listIcon"><FcStatistics/></ListItemIcon>
+            <ListItemText primary="Estatísticas e Relatorios"/>
+          </ListItem>
+
+
+          <ListItem className="listItem" id="logoutItem" button onClick={() => alert('Funcionalidade em desenvolvimento')}>
+            <ListItemIcon className="listIcon"><FiLogOut/></ListItemIcon>
+            <ListItemText primary="Sair do Sistema"/>
+          </ListItem>
+
+
+
 
         </List>
       </Drawer>
